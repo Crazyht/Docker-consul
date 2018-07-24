4a7ded9c2082d2f8cb8be48c8ff97d0fd16b3449
TINANT Hervé <herve.tinant@finquest.com>
2018-07-24 22:46:19 +0200

    Add build.cmd

---
+++ build.cmd
@@ -0,0 +1,3 @@
+docker build -t crazyht/alpine-s6:latest ./alpine-s6/
+docker build -t crazyht/alpine-consul:latest ./alpine-consul/
+docker build -t crazyht/alpine-consul-template:latest ./alpine-consul-template/
