ea84a446feca1b85ad8c55b93a5a052c2ab0ffdd
TINANT Hervé <herve.tinant@finquest.com>
2018-07-27 09:27:52 +0200

    update local build batch file

--- build.cmd
+++ build.cmd
@@ -1,3 +1,5 @@
 docker build -t crazyht/alpine-s6:latest ./alpine-s6/
 docker build -t crazyht/alpine-consul:latest ./alpine-consul/
 docker build -t crazyht/alpine-consul-template:latest ./alpine-consul-template/
+docker build -t crazyht/alpine-consul-redis:latest ./alpine-consul-redis/
+docker build -t crazyht/alpine-consul-redis-sentinel:latest ./alpine-consul-redis-sentinel/
