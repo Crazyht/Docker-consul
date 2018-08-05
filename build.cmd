3f865c440840397252a2b69d62d988929a43d359
TINANT Hervé <herve.tinant@finquest.com>
2018-08-05 16:56:41 +0200

    Add build process
    Fix automated build process 
    
    
    MIgrate to travis ci build
    
    
    add exec permission to build/*
    
    
    Try fix Travis-CI build script
    
    
    Fix docker compose for Travis
    
    
    Try fix export variables
    
    
    Try fix build
    
    
    Add full description on deploy latest

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
-docker build -t crazyht/alpine-s6:latest ./alpine-s6/
-docker build -t crazyht/alpine-consul:latest ./alpine-consul/
-docker build -t crazyht/alpine-consul-ui:latest ./alpine-consul-ui/
-docker build -t crazyht/alpine-consul-template:latest ./alpine-consul-template/
-docker build -t crazyht/alpine-consul-redis:latest ./alpine-consul-redis/
-docker build -t crazyht/alpine-consul-redis-sentinel:latest ./alpine-consul-redis-sentinel/
+docker build --build-arg CACHE_TAG="local" --build-arg SOURCE_COMMIT="nocommit" --build-arg SOURCE_BRANCH="LocalDev" -t crazyht/alpine-s6:latest ./alpine-s6/
+docker build --build-arg CACHE_TAG="local" --build-arg SOURCE_COMMIT="nocommit" --build-arg SOURCE_BRANCH="LocalDev" -t crazyht/alpine-consul:latest ./alpine-consul/
+docker build --build-arg CACHE_TAG="local" --build-arg SOURCE_COMMIT="nocommit" --build-arg SOURCE_BRANCH="LocalDev" -t crazyht/alpine-consul-ui:latest ./alpine-consul-ui/
+docker build --build-arg CACHE_TAG="local" --build-arg SOURCE_COMMIT="nocommit" --build-arg SOURCE_BRANCH="LocalDev" -t crazyht/alpine-consul-template:latest ./alpine-consul-template/
+docker build --build-arg CACHE_TAG="local" --build-arg SOURCE_COMMIT="nocommit" --build-arg SOURCE_BRANCH="LocalDev" -t crazyht/alpine-consul-redis:latest ./alpine-consul-redis/
+docker build --build-arg CACHE_TAG="local" --build-arg SOURCE_COMMIT="nocommit" --build-arg SOURCE_BRANCH="LocalDev" -t crazyht/alpine-consul-redis-sentinel:latest ./alpine-consul-redis-sentinel/
