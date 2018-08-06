e3729f4b95b0d3648cfbc415e2e407dfb8662674
TINANT Hervé <herve.tinant@finquest.com>
2018-08-06 22:15:39 +0200

    Update to redis 4.0.11 & Update win build script

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
-docker build --build-arg CACHE_TAG="local" --build-arg SOURCE_COMMIT="nocommit" --build-arg SOURCE_BRANCH="LocalDev" -t crazyht/alpine-s6:latest ./alpine-s6/
-docker build --build-arg CACHE_TAG="local" --build-arg SOURCE_COMMIT="nocommit" --build-arg SOURCE_BRANCH="LocalDev" -t crazyht/alpine-consul:latest ./alpine-consul/
-docker build --build-arg CACHE_TAG="local" --build-arg SOURCE_COMMIT="nocommit" --build-arg SOURCE_BRANCH="LocalDev" -t crazyht/alpine-consul-ui:latest ./alpine-consul-ui/
-docker build --build-arg CACHE_TAG="local" --build-arg SOURCE_COMMIT="nocommit" --build-arg SOURCE_BRANCH="LocalDev" -t crazyht/alpine-consul-template:latest ./alpine-consul-template/
-docker build --build-arg CACHE_TAG="local" --build-arg SOURCE_COMMIT="nocommit" --build-arg SOURCE_BRANCH="LocalDev" -t crazyht/alpine-consul-redis:latest ./alpine-consul-redis/
-docker build --build-arg CACHE_TAG="local" --build-arg SOURCE_COMMIT="nocommit" --build-arg SOURCE_BRANCH="LocalDev" -t crazyht/alpine-consul-redis-sentinel:latest ./alpine-consul-redis-sentinel/
+docker build -t crazyht/alpine-s6:dev ./alpine-s6/
+docker build -t crazyht/alpine-consul:dev ./alpine-consul/
+docker build -t crazyht/alpine-consul-ui:dev ./alpine-consul-ui/
+docker build -t crazyht/alpine-consul-template:dev ./alpine-consul-template/
+docker build -t crazyht/alpine-consul-redis:dev ./alpine-consul-redis/
+docker build -t crazyht/alpine-consul-redis-sentinel:dev ./alpine-consul-redis-sentinel/
